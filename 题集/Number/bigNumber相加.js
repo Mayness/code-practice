a572ee7cbe8b0a3367385b35c9130a82556aef70
mayness <1095346833@qq.com>
2019-10-07 21:07:04 +0800

    fix: 修改

--- 题集/Number/bigNumber相加.js
+++ 题集/Number/bigNumber相加.js
@@ -13,36 +13,3 @@ function sumStrings(a = '0', b = '0') {
 }
 
 sumStrings('12654272432143241235223', '32415475684524');
-
-
-//  另外一道题 +1 一样的原理
-// 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
-
-// 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
-
-// 你可以假设除了整数 0 之外，这个整数不会以零开头。
-
-// 示例 1:
-
-// 输入: [1,2,3]
-// 输出: [1,2,4]
-// 解释: 输入数组表示数字 123。
-// 示例 2:
-
-// 输入: [4,3,2,1]
-// 输出: [4,3,2,2]
-// 解释: 输入数组表示数字 4321。
-/**
- * @param {number[]} digits
- * @return {number[]}
- */
-var plusOne = function(digits) {
-  let carry = null;
-  let result = '';
-  while(digits.length || carry) {
-      carry = ~~digits.pop() + (carry === null ? 1 : carry)
-      result = carry%10 + result;
-      carry = Math.floor(carry/10);
-  }
-  return result.split('');
-};
