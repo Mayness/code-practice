ebe32db0bd9c4f6d933628e75577972cb97a41dd
mayness <1095346833@qq.com>
2020-03-31 19:10:10 +0800

    feat: 增加题解

--- group/test.js
+++ group/test.js
@@ -1,4 +1,29 @@
-    1
-  2  
- 3   
-4
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0;
+  let queue = [ root ];
+  let depth = 1;
+  while(true) {
+    const temp = [];
+    for (let item of queue) {
+      if (item.left) temp.push(item.left);
+      if (item.right) temp.push(item.right);
+    }
+    if (temp.length) {
+      depth++;
+      queue = temp;
+    } else {
+      return depth;
+    }
+  }
+};
